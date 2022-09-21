abba71056b62a3b0e823665775dcce291e0e2713
aurumbright <aurum.elean@gmail.com>
2022-09-20 21:05:45 -0700

    fixed errors and updated models and routes

--- seeds/index.js
+++ seeds/index.js
@@ -6,17 +6,10 @@ const userData = require('./user-seed');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
 
   await seedGoals();
 

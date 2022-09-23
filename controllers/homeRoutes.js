3252566b757144b43259c7ae04d437d337c56f63
aurumbright <aurum.elean@gmail.com>
2022-09-22 20:05:22 -0700

    single goal GET request for review

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,19 +2,26 @@ const router = require('express').Router();
 const { Goal, Todo } = require('../models');
 // const withAuth = require('../utils/auth');
 
-router.get('/goals/:goal_id', async (req, res) => {
+// single goal GET route
+router.get('/goals/:id', async (req, res) => {
   try {
-    const goalData = await Goal.findByPk(req.params.goal_id, {
-      include: [
-        {
-          model: Todo,
-          attributes: ['todo_name'],
-        }
-      ]
+    const goalData = await Goal.findByPk(req.params.id, {
+      include: [{ model: Todo }],
+      attributes: {
+        include: [
+          [
+            sequelize.literal(
+              '(SELECT * FROM todo WHERE todo.goal_id = goal.id)'
+            ),
+          ],
+          // I'm not 100% certain this is the correct location for this
+          'todo_name',
+        ],
+      },
     });
     const goal = goalData.get({ plain: true });
-    //Need to make handlebar page and then put name below in render spot
-    res.render('single view handlebar name', {
+    //Need to make handlebar page and then put name below in render spot - DONE
+    res.render('single-goal', {
       ...goal,
       logged_in: req.session.logged_in,
     });
@@ -22,9 +29,5 @@ router.get('/goals/:goal_id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-
-
-
 
 module.exports = router;
